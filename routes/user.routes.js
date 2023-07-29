d68a8866d30f0aaf9e1eeacaf73b0603d987a403
rach <rachelpytse@gmail.com>
2023-07-29 12:06:45 +0200

    cloudinary + GET /user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const SignupController = require("../controllers/auth/signup.controller");
 const LoginController = require("../controllers/auth/login.controller");
+const fileUploader = require("../config/cloudinary.config");
+const User = require("../models/User.model");
+const mongoose = require("mongoose");
 
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
@@ -11,13 +14,38 @@ router.post("/users", SignupController.createSignup);
 // POST  /sessions - Verifies email and password and returns a JWT
 router.post("/sessions", LoginController.login);
 
-// GET  /session  -  Used to verify JWT stored on the client
-router.get("/session", isAuthenticated, (req, res, next) => {
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and is made available on `req.payload`
+router.get("/user", isAuthenticated, (req, res, next) => {
+  User.findById(req.payload.user._id)
+    .then((userFromDB) => {
+      res.json(userFromDB);
+    })
+    .catch((error) => next(error));
+});
 
-  // Send back the token payload object containing the user data
-  res.status(200).json(req.payload);
+router.post(
+  "/upload",
+  isAuthenticated,
+  fileUploader.single("avatar"),
+  (req, res, next) => {
+    console.log("file is: ", req.file);
+
+    if (!req.file) {
+      next(new Error("No file uploaded!"));
+      return;
+    }
+
+    res.json({ fileUrl: req.file.path });
+  }
+);
+
+router.put("/user", isAuthenticated, (req, res, next) => {
+  User.findByIdAndUpdate(
+    req.payload.user._id,
+    { avatar: req.body.avatar },
+    { new: true }
+  )
+    .then((updatedUser) => res.json(updatedUser))
+    .catch((error) => next(error));
 });
 
 module.exports = router;
