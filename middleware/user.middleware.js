090816a7b5f26609fc58861ce6ad2add3c2f6c1a
Cedric Aim <cedric.aim@gmail.com>
2023-08-04 23:41:02 +0200

    bug fixed on extractUser middleware

--- middleware/user.middleware.js
+++ middleware/user.middleware.js
@@ -5,8 +5,7 @@ function extractUser(req, res, next) {
   const token = getTokenFromHeaders(req);
   if (token) {
     jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, data) => {
-      if (err) return res.status(403).json({ message: 'Token is not valid' });
-      req.user = data.user;
+      if (!err) req.user = data.user;
     });
   }
   next();
