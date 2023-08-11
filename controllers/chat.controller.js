bc3bf864e9f24523cbcd5bac9e1803624e11e2c9
Cedric Aim <cedric.aim@gmail.com>
2023-08-11 17:28:22 +0200

    adjustement on conversation token

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,34 +1,21 @@
 const EventService = require('../services/event.service');
 const eventServiceInstance = new EventService();
 
-async function init(req, res, next) {
-  try {
-    //get the user from the middleware if present
-    const session = { id: req.sessionID, content: req.session };
-    const response = await eventServiceInstance.events({ type: 'INIT' }, session, null);
-
-    console.log('Session ID output : ', req.sessionID);
-    return res.status(200).json(response);
-  } catch (err) {
-    next(err);
-  }
-}
-
 async function events(req, res, next) {
   try {
     //get the user from the middleware if present
     const userId = req.user ? req.user._id : null;
     const session = { id: req.sessionID, content: req.session };
-    console.log('conversationToken:', req.conversationToken);
+    console.log('conversationToken before:', req.conversationToken);
     const conversationToken = req.conversationToken || req.sessionID;
 
     const conversation = await eventServiceInstance.events(req.body, session, userId);
 
-    console.log('Session ID output : ', req.sessionID);
+    console.log('conversationToken after:', conversationToken);
     return res.status(200).json({ token: conversationToken, conversation });
   } catch (err) {
     next(err);
   }
 }
 
-module.exports = { events, init };
+module.exports = { events };
--- routes/chat.routes.js
+++ routes/chat.routes.js
@@ -4,7 +4,6 @@ const ChatController = require('../controllers/chat.controller');
 const { extractUser, extractConversationToken } = require('../middleware/user.middleware');
 
 // POST /chat  - Send the event and conversation to the chat api
-router.get('/chat/events', extractUser, ChatController.init);
 router.post('/chat/events', extractUser, extractConversationToken, ChatController.events);
 
 module.exports = router;
