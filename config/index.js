bf87d007a3332f9a0fd6c6fae07bdf2b9982af36
Cedric Aim <cedric.aim@gmail.com>
2023-08-18 16:49:30 +0200

    add config for accepting netlify preview

--- config/index.js
+++ config/index.js
@@ -15,6 +15,8 @@ const cors = require('cors');
 
 const FRONTEND_URLS = [process.env.REMOTE_ORIGIN, process.env.LOCAL_ORIGIN || 'http://localhost:8000'];
 
+console.log('FRONTEND_URLS >> ', FRONTEND_URLS);
+
 // Middleware configuration
 module.exports = app => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
